ea20e5a49c0f167abdf1c829c1493c1b97d397f5
vitsmart <vitsmart2@gmail.com>
2022-09-15 09:04:19 +0100

    Dialog modal

--- my-noble-app/src/components/operations.js
+++ my-noble-app/src/components/operations.js
@@ -1,5 +1,29 @@
+import React, { useState } from "react";
+import { Dialog } from "@reach/dialog";
+import "@reach/dialog/styles.css";
 
-const Operations = () => {
+const Operations = (props) => {
+
+  const [showDialog, setShowDialog] = useState(false);
+const openTab1 = () => setShowDialog(true);
+const closeTab1 = () => setShowDialog(false);
+const openTab2 = () => setShowDialog(true);
+const closeTab2 = () => setShowDialog(false);
+const openTab3 = () => setShowDialog(true);
+const closeTab3 = () => setShowDialog(false);
+
+// const openTab1 = () => {
+//   const dialog = document.getElementById("tabDialog_1");
+//   dialog.showModal();
+//   console.log("tab1");
+// };
+// const closeTab1 = () => {
+//   const dialog = document.getElementById("tabDialog_1");
+//   dialog.close();
+//   console.log("closing");
+// }
+
+
 
 
 
@@ -14,8 +38,15 @@ const Operations = () => {
     </div>
 
     <div className="operations">
+    <button
+          className="btn operations__tab operations__tab--1"
+        onClick={openTab1}>
+          <span>01</span>Instant Transfers
+        </button>
+
+      <Dialog style={{color: "white"}} isOpen={showDialog} onDismiss={closeTab1} className="operations__Dialog">
       <div
-        className="operations__content operations__content--1"
+        className="operations__content--1"
       >
           <h5 className="operations__header">
           Transfer money to anyone, instantly! No fees, no BS.
@@ -26,18 +57,16 @@ const Operations = () => {
           ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat.
         </p>
-        <button
-          className="btn operations__tab operations__tab--1"
-          data-tab="1"
-        >
-          <span>01</span>Instant Transfers
-        </button>
+       <span aria-hidden className="formDialog__closeBtn" onClick={closeTab1}>x</span>
         </div>
+        </Dialog>
 
+        <button className="btn operations__tab operations__tab--2" data-tab="2" onClick={openTab2}>
+          <span>02</span>Instant Loans
+        </button>
 
-
-
-<div className="operations__content operations__content--2">
+<Dialog style={{color: "white"}} isOpen={showDialog} onDismiss={closeTab2} className="operations__Dialog">
+  <div className="operations__content--2">
        
        <h5 className="operations__header">
          Buy a home or make your dreams come true, with instant loans.
@@ -48,11 +77,18 @@ const Operations = () => {
          cupidatat non proident, sunt in culpa qui officia deserunt mollit
          anim id est laborum.
        </p>
-        <button className="btn operations__tab operations__tab--2" data-tab="2">
-          <span>02</span>Instant Loans
-        </button>
+       <span aria-hidden className="formDialog__closeBtn" onClick={closeTab2}>x</span>
 </div>
-     
+</Dialog>
+
+
+
+<button className="btn operations__tab--3" data-tab="3" onClick={openTab3}>
+          <span>03</span>Instant Closing
+        </button>
+      
+        <Dialog style={{color: "white"}} isOpen={showDialog} onDismiss={closeTab3} className="operations__Dialog">
+<div>
       <div className="operations__content operations__content--3">
        
         <h5 className="operations__header">
@@ -64,20 +100,15 @@ const Operations = () => {
           veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
           ea commodo consequat.
         </p>
-        <button className="btn operations__tab operations__tab--3" data-tab="3">
-          <span>03</span>Instant Closing
-        </button>
+        <span aria-hidden className="formDialog__closeBtn" onClick={closeTab3}>x</span>
+       </div>
       </div>
-     
+      </Dialog>
       </div>
     
   </section>
     )
 };
- {/* <div className="operations__icon operations__icon--3">
-          <svg>
-            <use xlink:href="img/icons.svg#icon-user-x"></use>
-          </svg>
-        </div> */}
+ 
 
 export default Operations;
