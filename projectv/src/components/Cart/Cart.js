31708255207847850589441dc8124315d7758ff3
vitsmart <vitsmart2@gmail.com>
2022-09-21 11:59:31 +0100

    updating modal

--- projectv/src/components/Cart/Cart.js
+++ projectv/src/components/Cart/Cart.js
@@ -1,16 +1,48 @@
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux/';
 import CartItem from './CartItems';
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+
 
 const Cart = (props) => {
 const cartItems = useSelector(state => state.cart.items)
 //const hasItems = () => { return cartItems.length > 0};
  const Totalprice = useSelector(state => state.cart.totalprice) 
-
-
-return (
-    <>
-    <div className='wrapper'>
-      <h2>My Shopping Cart</h2>
+ console.log(Totalprice);
+  const style = {
+   position: 'absolute',
+   top: '50%',
+   left: '50%',
+   transform: 'translate(-50%, -50%)',
+   width: 400,
+   bgcolor: 'background.paper',
+   border: '2px solid #000',
+   boxShadow: 24,
+   p: 4,
+ };
+ 
+ 
+   const [open, setOpen] = useState(false);
+   const handleOpen = () => setOpen(true);
+   const handleClose = () => setOpen(false);
+ 
+   return (
+     <div>
+       <Button onClick={handleOpen}>See Cart</Button>
+       <Modal
+         open={open}
+         onClose={handleClose}
+         aria-labelledby="modal-modal-title"
+         aria-describedby="modal-modal-description"
+       >
+         <Box sx={style}>
+           <Typography id="modal-modal-title" variant="h6" component="h2">
+           <h2>My Shopping Cart</h2>
       <ul>
         {cartItems.map((item) => (
  <CartItem
@@ -26,16 +58,20 @@ return (
         ))}
       </ul>
       <div className="actions">
-        <button className='button--alt' onClick={props.onClose}>
+        <button className='button--alt' onClick={handleClose}>
           Close
         </button>
         <span className='badge'>{Totalprice}</span>
         <button className="button">Buy Now</button>
-      </div>
-      </div>
-      </>
-  );
-};
+</div>
 
+           </Typography>
+          
+         </Box>
+       </Modal>
+     </div>
+   );
+ }
+ 
 
 export default Cart;
